73895ed5b17b995a988a2204bc63df8a496d4bd4
kunwe <1508974740@qq.com>
2016-10-13 13:51:57 +0800

    添加 BaseActivity

---
+++ app/src/main/java/com/china/shopping/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.china.shopping.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kunwe on 2016/10/13.
+ * 初始化 activity  删除测试my
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/china/shopping/activity/My.java
+++
@@ -1,8 +0,0 @@
-package com.china.shopping.activity;
-
-/**
- * Created by cds on 2016/10/13.
- */
-
-public class My {
-}
